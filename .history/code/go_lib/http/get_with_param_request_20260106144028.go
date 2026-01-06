658d59190f154c817c52ab5a6754b3847883ec9b
kongjihao <2981310451@qq.com>
2026-01-06 16:39:15 +0800

    http lib example

--- .history/code/go_lib/http/get_with_param_request_20260106144028.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import (
-	
-
-//关于GET请求的参数需要使用Go语言内置的net/url这个标准库来处理。
--- .history/code/go_lib/http/get_with_param_request_20260106144032.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-	"net/http"
-	"net/url"
-)
-
-//关于GET请求的参数需要使用Go语言内置的net/url这个标准库来处理。
